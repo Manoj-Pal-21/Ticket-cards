f98748094f19a526a7493993fa0dd541ddfbd07a
Manoj-Pal-21 <manojpal211999@gmail.com>
2022-03-26 11:46:32 +0530

    ticket-card

---
+++ src/App.js
@@ -0,0 +1,94 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="bg-white rounded-1 shadow-sm p-3 mb-3 w-100 mr-3">
+        <div className="d-flex align-items-center mb-2">
+          <small className="text-muted">A/C Sleeper (2+1)</small>
+          <small className="text-success ml-auto f-50"> CONFIRMED</small>
+        </div>
+        <h6 className="mb-3 l-hght-18 font-weight-bold text-dark">Osahan Travellers ISO 9002- 2009 Certified</h6>
+        <div className="row mx-0 mb-3">
+          <div className="col-6 p-0">
+            <small className="text-muted mb-1 f-10 pr-1">GOING FROM</small>
+            <p className="small mb-0 1-hght-14"> LUDHIANA</p>
+          </div>
+          <div className="col-6 p-0">
+            <small className="text-muted mb-1 f-10 pr-1">TO</small>
+            <p className="small mb-0 1-hght-14"> GOA</p>
+          </div>
+        </div>
+        <div className="row mx-0">
+          <div className="col-6 p-0">
+            <small className="text-muted mb-1 f-10 pr-1"> DATE OF JOURNEY</small>
+            <p className="small mb-0 1-hght-14"> 05 MAY,2022</p>
+          </div>
+          <div className="col-6 p-0">
+            <small className="text-muted mb-1 f-10 pr-1"> YOU RATED</small>
+            <p className="small small-1 mb-0 1-hght-14"> RATE NOW</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-white rounded-1 shadow-sm p-3 mb-3 w-100 mr-3">
+        <div className="d-flex align-items-center mb-2">
+          <small className="text-muted">A/C Sleeper (2+1)</small>
+          <small className="text-success ml-auto f-10"> CONFIRMED</small>
+        </div>
+        <h6 className="mb-3 l-hght-18 font-weight-bold text-dark">Osahan Travellers ISO 9002- 2009 Certified</h6>
+        <div className="row mx-0 mb-3">
+          <div className="col-6 p-0">
+            <small className="text-muted mb-1 f-10 pr-1">GOING FROM</small>
+            <p className="small mb-0 1-hght-14"> LUDHIANA</p>
+          </div>
+          <div className="col-6 p-0">
+            <small className="text-muted mb-1 f-10 pr-1">TO</small>
+            <p className="small mb-0 1-hght-14"> GOA</p>
+          </div>
+        </div>
+        <div className="row mx-0">
+          <div className="col-6 p-0">
+            <small className="text-muted mb-1 f-10 pr-1"> DATE OF JOURNEY</small>
+            <p className="small mb-0 1-hght-14"> 05 MAY,2022</p>
+          </div>
+          <div className="col-6 p-0">
+            <small className="text-muted mb-1 f-10 pr-1"> YOU RATED</small>
+            <p className="small small-1 mb-0 1-hght-14"> RATE NOW</p>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="bg-white rounded-1 shadow-sm p-3 mb-3 w-100">
+        <div className="d-flex align-items-center mb-2">
+          <small className="text-muted">A/C Sleeper (2+1)</small>
+          <small className="text-success ml-auto f-10"> CONFIRMED</small>
+        </div>
+        <h6 className="mb-3 l-hght-18 font-weight-bold text-dark">Osahan Travellers ISO 9002- 2009 Certified</h6>
+        <div className="row mx-0 mb-3">
+          <div className="col-6 p-0">
+            <small className="text-muted mb-1 f-10 pr-1">GOING FROM</small>
+            <p className="small mb-0 1-hght-14"> LUDHIANA</p>
+          </div>
+          <div className="col-6 p-0">
+            <small className="text-muted mb-1 f-10 pr-1">TO</small>
+            <p className="small mb-0 1-hght-14"> GOA</p>
+          </div>
+        </div>
+        <div className="row mx-0">
+          <div className="col-6 p-0">
+            <small className="text-muted mb-1 f-10 pr-1"> DATE OF JOURNEY</small>
+            <p className="small mb-0 1-hght-14"> 05 MAY,2022</p>
+          </div>
+          <div className="col-6 p-0">
+            <small className="text-muted mb-1 f-10 pr-1"> YOU RATED</small>
+            <p className="small small-1 mb-0 1-hght-14"> RATE NOW</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
